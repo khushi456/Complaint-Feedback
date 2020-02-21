3605afb12ea7a37757e46547290551b68f99d74c
Sakshi Maheshwari <sakshimaheshwari10@gmail.com>
2020-02-21 22:32:34 +0530

    Student_ui

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -1,9 +1,46 @@
 import React , { Component } from "react";
+import { Form,Button, Container ,Row ,Col,Accordion,Card} from 'react-bootstrap';
 
 export class Feedback extends Component{
     render(){
         return(
             <>
+            <Container style={{paddingTop:"8%"}}>   
+                  <Accordion defaultActiveKey="0">
+                    <Card>
+                        <Accordion.Toggle as={Card.Header} variant="" eventKey="0">
+                            <h4>Feedback Form </h4>
+                        </Accordion.Toggle>
+                        <Accordion.Collapse eventKey="0">
+                        <Card.Body>
+                          <Form>
+                          <Form.Group as={Row} controlId="complaintId">
+                            <Form.Label column sm={3}>
+                              <h6>Complaint Id </h6>
+                            </Form.Label>
+                            <Col sm={6}>
+                              <Form.Control type ="text"  required/>
+                            </Col>
+                          </Form.Group>
+                          <Form.Group as={Row} controlId="feedbackDesc">
+                            <Form.Label column sm={3}>
+                              <h6>Feedback </h6>
+                            </Form.Label>
+                            <Col sm={6}>
+                              <Form.Control as="textarea" rows="3" required/>
+                            </Col>
+                          </Form.Group>
+                          <Form.Group as={Row}>
+                            <Col sm={{ span: 10, offset: 3 }}>
+                              <Button variant="outline-secondary" type="submit">Submit</Button>{' '}
+                            </Col>
+                          </Form.Group>
+                        </Form>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>       
+            </Container>
             </>
             );
         }
